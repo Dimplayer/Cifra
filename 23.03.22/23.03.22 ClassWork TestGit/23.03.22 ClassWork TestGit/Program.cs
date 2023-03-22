91f4f2335daa2ec58eb896d855e0c592bdf626f2
NikSi039 <sidorova9999@internet.ru>
2023-03-22 19:42:16 +0200

    add project

---
+++ 23.03.22/23.03.22 ClassWork TestGit/23.03.22 ClassWork TestGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _23._03._22_ClassWork_TestGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
